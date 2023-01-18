688f97cb6ae97d8fb2cfbaf26b2b366d8adeac30
Suraj Pandey <surajpandey0094@gmail.com>
2023-01-18 19:39:18 +0530

    fixing issues

--- src/comp/Navbar/Navbar.js
+++ src/comp/Navbar/Navbar.js
@@ -4,22 +4,22 @@ import "../Navbar/Navbar.css"
 function Navbar() {
   return (
 
-    <nav class="navbar navbar-default navbar-fixed-top">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
+    <nav className="navbar navbar-default navbar-fixed-top">
+      <div className="container">
+        <div className="navbar-header">
+          <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span className="sr-only">Toggle navigation</span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="/">FiTnEsS FrEaK</a>
+          <a className="navbar-brand" href="/">FiTnEsS FrEaK</a>
         </div>
 
 
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 
-          <ul class="nav navbar-nav navbar-right">
+          <ul className="nav navbar-nav navbar-right">
             <li><a href="/">Home</a></li>
             <li><a href="/">about</a></li>
             <li><a href="/">portfolio</a></li>
